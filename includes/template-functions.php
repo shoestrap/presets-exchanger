a0edae74aca94d11c9e49444c14855109b3cf225
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-05 21:46:46 +0200

    including the description in the template function

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -10,6 +10,12 @@ function wpmuio_preset_export_code() {
 		<div class="col-md-6"><?php the_post_thumbnail( 'large' ); ?></div>
 	<?php endif; ?>
 
+	<hr>
+
+	<?php echo $field_value['_wpmuio_export_description'][0]; ?>
+
+	<hr>
+
 	<?php if ( has_post_thumbnail() ) : ?><div class="col-md-6"><?php endif; ?>
 		<div class="alert alert-info">
 			<?php _e( 'Double-Click the text below to select it and then copy it to your dashboard. Once you do, you can import it to your Shoestrap theme from the import menu in the theme options.' ); ?>
@@ -17,6 +23,9 @@ function wpmuio_preset_export_code() {
 	<?php if ( has_post_thumbnail() ) : ?></div><?php endif; ?>
 
 	<pre><?php echo $field_value['_wpmuio_export_data'][0]; ?></pre>
+
+	<hr>
+
 	<?php
 
 }

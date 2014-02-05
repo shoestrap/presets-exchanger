9802440e1f8830dcfbc99e2dce762db9f4e78202
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-05 21:50:10 +0200

    minor formatting improvements

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -6,21 +6,25 @@ function wpmuio_preset_export_code() {
 	$field_value = get_post_meta( $post->ID );
 	?>
 
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="col-md-6"><?php the_post_thumbnail( 'large' ); ?></div>
-	<?php endif; ?>
-
 	<hr>
 
 	<?php echo $field_value['_wpmuio_export_description'][0]; ?>
 
-	<hr>
+	<?php if ( !empty( $field_value['_wpmuio_export_description'][0] ) ) : ?><hr><?php endif; ?>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="col-md-6"><?php the_post_thumbnail( 'large' ); ?></div>
+	<?php endif; ?>
+
 
 	<?php if ( has_post_thumbnail() ) : ?><div class="col-md-6"><?php endif; ?>
 		<div class="alert alert-info">
 			<?php _e( 'Double-Click the text below to select it and then copy it to your dashboard. Once you do, you can import it to your Shoestrap theme from the import menu in the theme options.' ); ?>
 		</div>
 	<?php if ( has_post_thumbnail() ) : ?></div><?php endif; ?>
+
+	<div class="clearfix"></div>
+	<hr>
 
 	<pre><?php echo $field_value['_wpmuio_export_data'][0]; ?></pre>
 

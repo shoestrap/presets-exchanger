b8ffea51c2cf3733bd884c3f030f7e7eadcd733d
Aristeides Stathopoulos <aristath@gmail.com>
2014-02-06 18:38:54 +0200

    almost functional now

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -1,46 +1,12 @@
 <?php
 
+
 function wpmuio_preset_export_code() {
 	global $post;
 
 	$field_value = get_post_meta( $post->ID );
-	?>
-
-	<hr>
-
-	<?php echo $field_value['_wpmuio_export_description'][0]; ?>
-
-	<?php if ( !empty( $field_value['_wpmuio_export_description'][0] ) ) : ?><hr><?php endif; ?>
-
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="col-md-6">
-			<?php
-				$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
-				echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '" >';
-				the_post_thumbnail( 'large' );
-				echo '</a>';
-			?>
-		</div>
-	<?php endif; ?>
-
-
-	<?php if ( has_post_thumbnail() ) : ?><div class="col-md-6"><?php endif; ?>
-		<div class="alert alert-info">
-			<?php _e( 'Double-Click the text below to select it and then copy it to your dashboard. Once you do, you can import it to your Shoestrap theme from the import menu in the theme options.' ); ?>
-		</div>
-	<?php if ( has_post_thumbnail() ) : ?></div><?php endif; ?>
-
-	<div class="clearfix"></div>
-	<hr>
-
-	<pre><?php echo $field_value['_wpmuio_export_data'][0]; ?></pre>
-
-	<hr>
-
-	<?php
-
+	echo $field_value['_wpmuio_export_data'][0];
 }
-add_action( 'shoestrap_single_pre_content', 'wpmuio_preset_export_code' );
 
 
 function wpmuio_loop_override() {
@@ -57,6 +23,25 @@ function preset_content_template() {
 
 	<article <?php post_class( array( 'col-md-4' ) ); ?>>
 		<div class="thumbnail">
+			<?php
+				// get the thumbnail URL
+				$thumb_url = wp_get_attachment_url( get_post_thumbnail_id() );
+				if ( $thumb_url == '' )
+					$thumb_url = SHOESTRAP_PRESETS_URL . '/assets/img/empty.png';
+
+				$args = array(
+					"url"       => $thumb_url,
+					"width"     => 691,
+					"height"    => 424,
+					"crop"      => true,
+					"retina"    => "",
+					"resize"    => true,
+				);
+				$image = shoestrap_image_resize( $args );
+
+				echo '<img src="' . $image['url'] . '" style="width: 100%; height: auto;" />';
+			?>
+
 			<?php do_action( 'shoestrap_in_article_top' ); ?>
 
 			<header style="height:3em; overflow: hidden;">
@@ -64,7 +49,9 @@ function preset_content_template() {
 			</header>
 
 			<div class="entry-summary">
-				<button class="btn btn-success btn-block" id="copy-button-<?php echo $post->ID; ?>" data-clipboard-text="<?php echo $fields['_wpmuio_export_data'][0]; ?>" title="Copy to Clickboard">Copy to Clipboard</button>
+				<div class="overflow-expand"><?php echo $fields['_wpmuio_export_description'][0]; ?></div>
+				<div class="clearfix" style="height:1em;"></div>
+				<button class="btn btn-success btn-block" id="copy-button-<?php echo $post->ID; ?>" data-clipboard-text='<?php the_permalink(); ?>' title="<?php _e( 'Copy URL to Clipboard' ); ?>"><?php _e( 'Copy URL to Clipboard' ); ?></button>
 				<script>var client = new ZeroClipboard( $("#copy-button-<?php echo $post->ID; ?>"), { moviePath: "<?php echo SHOESTRAP_PRESETS_URL; ?>/assets/js/ZeroClipboard.swf" } );</script>
 
 			</div>
@@ -75,6 +62,7 @@ function preset_content_template() {
 	</article>
 	<?php
 }
+
 
 function wpmuio_enqueue_scripts() {
 	if ( is_post_type_archive( 'presets' ) ) {
@@ -87,3 +75,27 @@ function wpmuio_enqueue_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wpmuio_enqueue_scripts' );
+
+
+function wpmuio_blankout_content() {
+	if ( is_singular( 'presets' ) ) {
+		add_action( 'shoestrap_page_header_override', function() {} );
+		remove_filter('template_include', array('Roots_Wrapping', 'wrap'), 99);
+		add_action( 'shoestrap_content_single_override', 'wpmuio_preset_export_code' );
+	}
+}
+add_action( 'wp', 'wpmuio_blankout_content' );
+
+function wpmuio_presets_explain_text() {
+	if ( is_post_type_archive( 'presets' ) ) : ?>
+		<div class="alert alert-info">
+			<p>You can create your own presets and submit them here by exporting your Redux Options as a file.
+			Then <a href="http://shoestrap.org/wp-admin/post-new.php?post_type=presets" class="btn btn-info">Create a new Preset</a></p>
+
+			<p>To import a preset to your own site, simply click on the <strong>Copy URL to Clipboard</strong> button and then from your own installation go to Presentation &rarr; Theme Options &rarr; Import from URL.
+			Paste the URL and hit "Import".
+			That's all!</p>
+		</div>
+	<?php endif;
+}
+add_action( 'shoestrap_pre_main', 'wpmuio_presets_explain_text' );
